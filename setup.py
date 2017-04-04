6f7dac54abe63f4d557a3c963b7f6d0b801da625
Anthony <antishkins@gmail.com>
2017-04-04 17:02:49 +1000

    Update setup.py
    > Added basic details to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup
 
-setup(
-    scripts = ['divvy'],
-    packages=['src']
-)
+setup(name='divvy',
+      version='0.1',
+      description='',
+      url='https://github.com/PhilippeMorere/divvy/',
+      author='PhilippeMorere',
+      author_email='',
+      license='MIT',
+      packages=['divvy'],
+      scripts = ['divvy'],
+      install_requires=[],
+      zip_safe=False)

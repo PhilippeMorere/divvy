8fb380595b71dec5d1ab574df305233cca7a7b2a
Philippe Morere <philippe.morere@gmail.com>
2017-11-23 15:30:24 +1100

    Syntax cleaning

--- divvy/ParallelTasks.py
+++ divvy/ParallelTasks.py
@@ -1,11 +1,12 @@
-from multiprocessing import Process, Queue, current_process, freeze_support
+from multiprocessing import Process, Queue
 from subprocess import call
 import shlex
+
 
 class ParallelTasks():
     def __init__(self):
         pass
-    
+
     def execute(self, cmd):
         """ Executes system call with proper arguments"""
         args = shlex.split(cmd)
@@ -25,26 +26,29 @@ class ParallelTasks():
             output.put(str(result))
 
     def run(self, tasks, noProcs=4):
-        """ Distributes the given tasks on available CPUs using a queue system """
+        """
+        Distributes the given tasks on available CPUs using a queue system
+        """
         # Create queues
         task_queue = Queue()
         done_queue = Queue()
-    
+
         # Submit tasks
         for task in tasks:
             task_queue.put(task)
         nbTasks = task_queue.qsize()
-    
+
         # Start worker processes
         for i in range(noProcs):
-            Process(target=self.worker, args=(task_queue, done_queue, None)).start()
-    
+            Process(target=self.worker,
+                    args=(task_queue, done_queue, None)).start()
+
         # Printing results on screen and completion
         print('Results:')
         for i in range(nbTasks):
-            r = done_queue.get()
+            _ = done_queue.get()
             print('\t(Progress: {:>4.4}%)'.format(100.0 * i / float(nbTasks)))
-    
+
         # Tell child processes to stop
         for i in range(noProcs):
             task_queue.put('STOP')
@@ -54,4 +58,3 @@ if __name__ == '__main__':
     pt = ParallelTasks()
     tasks = [("ls", "ls", "ls"), ("pwd")]
     pt.run(tasks, 3)
-
--- divvy/RunExperiment.py
+++ divvy/RunExperiment.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from multiprocessing import freeze_support
 import argparse
 import os
 import re
@@ -10,10 +9,13 @@ from ParallelTasks import ParallelTasks
 
 Possible_options = ["outdir", "workdir"]
 
+
 def checkNodeExists(config, name):
-    if not name in config:
-        print(r"Error: configuration file must include a \"{}\" node.".format(name))
+    if name not in config:
+        print(r"Error: configuration file must include a \"{}\" node.".
+              format(name))
         sys.exit()
+
 
 def checkConfig(config):
     """ Check config file for missing nodes """
@@ -21,21 +23,24 @@ def checkConfig(config):
     checkNodeExists(config, "options")
     checkNodeExists(config["options"], "outdir")
     checkNodeExists(config, "commands")
-    if not "repeat" in config:
+    if "repeat" not in config:
         config["repeat"] = 1
-    if not "cpus" in config:
+    if "cpus" not in config:
         config["cpus"] = 1
+
 
 def parseReplacements(config):
     """Parses the replacements of the program."""
     replacements = {}
     for key, val in config.items():
         if isinstance(val, str):
-            # Escape string so that it is not divided into multiple command arguments
+            # Escape string so that it is not divided into multiple command
+            # arguments
             replacements[key] = "\"{}\"".format(val)
         else:
             replacements[key] = val
     return replacements
+
 
 def parseCommands(config, replacements):
     """Parses the commands of the program."""
@@ -45,7 +50,7 @@ def parseCommands(config, replacements):
         if isinstance(val, (numbers.Number, str)):
             for i in range(len(commands)):
                 commands[i] = re.sub("\$\{%s\}" % key, str(val), commands[i])
-        else: # This is the case for trying several parameter values
+        else:  # This is the case for trying several parameter values
             newCommands = []
             for cmd in commands:
                 if cmd.find(key) == -1:
@@ -57,6 +62,7 @@ def parseCommands(config, replacements):
             commands = newCommands
 
     return commands
+
 
 def parseOptions(config, replacements):
     """Parses the options of the program."""
@@ -78,44 +84,49 @@ def parseOptions(config, replacements):
             sys.exit()
     return params
 
+
 def parseToTasks(config):
     """ Parses commands from config file to a list of tasks to be executed"""
-    replacements = parseReplacements(config["replacements"] if "replacements" in config else {})
+    replacements = parseReplacements(config["replacements"]
+                                     if "replacements" in config else {})
     params = parseOptions(config["options"], replacements)
     commands = parseCommands(config["commands"], replacements)
-    
+
     if params["workdir"] is not None:
         os.chdir(params["workdir"])
     return [tuple(commands)] * config["repeat"]
+
 
 def main():
     # Parse argument(s)
     parser = argparse.ArgumentParser()
     parser.add_argument('config', help='Experiment configuration file (YAML)')
     args = parser.parse_args()
-    
+
     # Check if config file exists
     if not os.path.isfile(args.config):
-        print("Error: configuration file {} does not exist.".format(args.config))
+        print("Error: configuration file {} does not exist.".
+              format(args.config))
         sys.exit()
-    
+
     # Parse Yaml file and verify config
     yamlTree = yaml.safe_load(open(args.config))
     config = yamlTree["experiment"]
     checkConfig(config)
-    
+
     # Parse Yaml config to task list
     tasks = parseToTasks(config)
 
-    #TODO: print info and start time
+    # TODO: print info and start time
     print("Starting experiment.")
-    
+
     # Run the taks in parallel
     pt = ParallelTasks()
     pt.run(tasks, config["cpus"])
 
     print("All done.")
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
